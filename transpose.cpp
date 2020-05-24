605acd569e13832c911744d9bfe7d5890e87ebeb
adshidtadka <nextage900fx@gmail.com>
2020-05-24 19:33:54 +0900

    LL用のtransposeを作成

--- transpose.cpp
+++ transpose.cpp
@@ -20,6 +20,28 @@ VVI transposeInt(VVI v) {
     return rlt;
 }
 
+vector<vector<LL>> transposeLL(vector<vector<LL>> v) {
+    LL p = 0;
+    vector<vector<LL>> rlt;
+
+    LL row = v.size();
+    LL col = v[0].size();
+
+    rlt.resize(col);
+    for (LL i = 0; col > i; i++) rlt[i].resize(row);
+
+    for (LL i = 0; row > i; i++) {
+        for (LL j = 0; col > j; j++) {
+            LL d = v[i][j];
+            LL x = p % col;
+            LL y = p / col;
+            rlt[x][y] = d;
+            p++;
+        }
+    }
+    return rlt;
+}
+
 vector<vector<char>> transposeChar(vector<vector<char>> v) {
     int p = 0;
     vector<vector<char>> rlt;
@@ -40,5 +62,3 @@ vector<vector<char>> transposeChar(vector<vector<char>> v) {
         }
     return rlt;
 }
-
-
